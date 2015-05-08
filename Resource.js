ffec7c5c46dc0051582d8a4f11e6d8cdc12266f1
biofractal <biofractal@gmail.com>
2015-05-08 16:15:26 +0100

    added support for array of before handlers

--- Resource.js
+++ Resource.js
@@ -54,7 +54,10 @@ module.exports = function(app, route, modelName, model) {
     register: function(app, method, path, callback, last, options) {
       var args = [path];
       if (options && options.before) {
-        args.push(options.before.bind(this));
+        before = [].concat options.before
+        for (var len = before.length, i=0; i<len; ++i) {
+          args.push(before[i].bind(this));
+        }
       }
       args.push(callback.bind(this));
       if (options && options.after) {
