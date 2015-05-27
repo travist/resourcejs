173507c94b30e31e7c4a18444e141ae3c6eb9651
Zack Urben <zackurben@gmail.com>
2015-05-26 23:55:26 -0500

    Updating test code to remove minor bugs and ensure code consistency.

--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
+'use strict';
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('supertest');
-var should = require('should');
 var assert = require('assert');
 var mongoose = require('mongoose');
 var Resource = require('../Resource');
@@ -37,6 +38,8 @@ describe('Connect to MongoDB', function() {
   it('Connect to MongoDB', function (done) {
     mongoose.connect('mongodb://localhost/test', done);
   });
+
+
   it('Drop test database', function(done) {
     mongoose.connection.db.dropDatabase(done);
   });
@@ -44,14 +47,17 @@ describe('Connect to MongoDB', function() {
 
 describe('Test full CRUD capabilities.', function() {
   var resource = {};
+
   it('/GET empty list', function(done) {
-    request(app).get('/test/resource')
+    request(app)
+      .get('/test/resource')
       .expect('Content-Range', '*/0')
       .expect(204)
       .end(function(err, res) {
         done(err);
       });
   });
+
   it('/POST Create new resource', function(done) {
     request(app)
       .post('/test/resource')
@@ -69,6 +75,7 @@ describe('Test full CRUD capabilities.', function() {
         done(err);
       });
   });
+
   it('/GET The new resource.', function(done) {
     request(app)
       .get('/test/resource/' + resource._id)
@@ -81,6 +88,7 @@ describe('Test full CRUD capabilities.', function() {
         done(err);
       });
   });
+
   it('/PUT Change data on the resource', function(done) {
     request(app)
       .put('/test/resource/' + resource._id)
@@ -141,7 +149,8 @@ describe('Test full CRUD capabilities.', function() {
   });
 
   it('/GET index of resources', function(done) {
-    request(app).get('/test/resource')
+    request(app)
+      .get('/test/resource')
       .expect('Content-Type', /json/)
       .expect('Content-Range', '0-0/1')
       .expect(200)
@@ -153,6 +162,7 @@ describe('Test full CRUD capabilities.', function() {
         done(err);
       });
   });
+
   it('/DELETE the resource', function(done) {
     request(app)
       .delete('/test/resource/' + resource._id)
@@ -161,6 +171,7 @@ describe('Test full CRUD capabilities.', function() {
         done(err);
       });
   });
+
   it('/GET empty list', function(done) {
     request(app).get('/test/resource')
       .expect('Content-Range', '*/0')
@@ -174,6 +185,7 @@ describe('Test full CRUD capabilities.', function() {
 describe('Test search capabilities', function() {
   it('Create a full index of resources', function(done) {
     var age = 0;
+
     async.whilst(
       function() { return age < 25; },
       function(cb) {
@@ -185,8 +197,7 @@ describe('Test search capabilities', function() {
             age: age
           })
           .end(function(err, res) {
-            console.log('Creating resource: Test Age ' + age);
-            resource = res.body;
+            var resource = res.body;
             assert.equal(resource.title, 'Test Age ' + age);
             assert.equal(resource.description, 'Description of test age ' + age);
             assert.equal(resource.age, age);
@@ -199,7 +210,8 @@ describe('Test search capabilities', function() {
   });
 
   it('Should limit 10', function(done) {
-    request(app).get('/test/resource')
+    request(app)
+      .get('/test/resource')
       .expect('Content-Type', /json/)
       .expect('Content-Range', '0-9/25')
       .expect(206)
@@ -217,7 +229,8 @@ describe('Test search capabilities', function() {
   });
 
   it('Should accept a change in limit', function(done) {
-    request(app).get('/test/resource?limit=5')
+    request(app)
+      .get('/test/resource?limit=5')
       .expect('Content-Type', /json/)
       .expect('Content-Range', '0-4/25')
       .expect(206)
@@ -235,7 +248,8 @@ describe('Test search capabilities', function() {
   });
 
   it('Should be able to skip and limit', function(done) {
-    request(app).get('/test/resource?limit=5&skip=4')
+    request(app)
+      .get('/test/resource?limit=5&skip=4')
       .expect('Content-Type', /json/)
       .expect('Content-Range', '4-8/25')
       .expect(206)
@@ -253,7 +267,8 @@ describe('Test search capabilities', function() {
   });
 
   it('Should be able to select fields', function(done) {
-    request(app).get('/test/resource?limit=10&skip=10&select=title,age')
+    request(app)
+      .get('/test/resource?limit=10&skip=10&select=title,age')
       .expect('Content-Type', /json/)
       .expect('Content-Range', '10-19/25')
       .expect(206)
@@ -271,7 +286,8 @@ describe('Test search capabilities', function() {
   });
 
   it('Should be able to sort', function(done) {
-    request(app).get('/test/resource?select=age&sort=-age')
+    request(app)
+      .get('/test/resource?select=age&sort=-age')
       .expect('Content-Type', /json/)
       .expect('Content-Range', '0-9/25')
       .expect(206)
@@ -289,7 +305,8 @@ describe('Test search capabilities', function() {
   });
 
   it('Should paginate with a sort', function(done) {
-    request(app).get('/test/resource?limit=5&skip=5&select=age&sort=-age')
+    request(app)
+      .get('/test/resource?limit=5&skip=5&select=age&sort=-age')
       .expect('Content-Type', /json/)
       .expect('Content-Range', '5-9/25')
       .expect(206)
@@ -307,7 +324,8 @@ describe('Test search capabilities', function() {
   });
 
   it('Should be able to find', function(done) {
-    request(app).get('/test/resource?limit=5&select=age&age=5')
+    request(app)
+      .get('/test/resource?limit=5&select=age&age=5')
       .expect('Content-Type', /json/)
       .expect('Content-Range', '0-0/1')
       .expect(200)
@@ -321,7 +339,8 @@ describe('Test search capabilities', function() {
   });
 
   it('$lt selector', function(done) {
-    request(app).get('/test/resource?age__lt=5')
+    request(app)
+      .get('/test/resource?age__lt=5')
       .expect('Content-Range', '0-4/5')
       .end(function(err, res) {
         assert.equal(res.body.length, 5);
@@ -333,7 +352,8 @@ describe('Test search capabilities', function() {
   });
 
   it('$lte selector', function(done) {
-    request(app).get('/test/resource?age__lte=5')
+    request(app)
+      .get('/test/resource?age__lte=5')
       .expect('Content-Range', '0-5/6')
       .end(function(err, res) {
         assert.equal(res.body.length, 6);
@@ -345,7 +365,8 @@ describe('Test search capabilities', function() {
   });
 
   it('$gt selector', function(done) {
-    request(app).get('/test/resource?age__gt=5')
+    request(app)
+      .get('/test/resource?age__gt=5')
       .expect('Content-Range', '0-9/19')
       .end(function(err, res) {
         assert.equal(res.body.length, 10);
@@ -357,7 +378,8 @@ describe('Test search capabilities', function() {
   });
 
   it('$gte selector', function(done) {
-    request(app).get('/test/resource?age__gte=5')
+    request(app)
+      .get('/test/resource?age__gte=5')
       .expect('Content-Range', '0-9/20')
       .end(function(err, res) {
         assert.equal(res.body.length, 10);
@@ -369,7 +391,8 @@ describe('Test search capabilities', function() {
   });
 
   it('regex selector', function(done) {
-    request(app).get('/test/resource?title__regex=/.*Age [0-1]?[0-3]$/g')
+    request(app)
+      .get('/test/resource?title__regex=/.*Age [0-1]?[0-3]$/g')
       .expect('Content-Range', '0-7/8')
       .end(function(err, res) {
         var valid = [0, 1, 2, 3, 10, 11, 12, 13];
@@ -383,9 +406,7 @@ describe('Test search capabilities', function() {
 });
 
 describe('Test after handlers', function() {
-
   it('Should call the global after handlers', function(done) {
-
     // Create the REST resource.
     Resource(app, '/test2', 'resource', ResourceModel).rest({
       after: function(req, res, next) {
@@ -398,29 +419,29 @@ describe('Test after handlers', function() {
     });
 
     request(app)
-        .post('/test2/resource')
-        .send({
-            title: "Test1",
-            description: "12345678"
-        })
+    .post('/test2/resource')
+    .send({
+      title: "Test1",
+      description: "12345678"
+    })
+    .expect('Content-Type', /json/)
+    .expect(201)
+    .end(function(err, res) {
+      var resource = res.body;
+      assert.equal(resource.title, 'Test1');
+      assert.equal(resource.description, '12345678');
+      assert(resource.hasOwnProperty('_id'), 'Resource ID not found');
+
+      request(app)
+        .get('/test2/resource/' + resource._id)
         .expect('Content-Type', /json/)
-        .expect(201)
+        .expect(200)
         .end(function(err, res) {
-            resource = res.body;
-            assert.equal(resource.title, 'Test1');
-            assert.equal(resource.description, '12345678');
-            assert(resource.hasOwnProperty('_id'), 'Resource ID not found');
-            request(app)
-              .get('/test2/resource/' + resource._id)
-              .expect('Content-Type', /json/)
-              .expect(200)
-              .end(function(err, res) {
-                assert.equal(res.body.title, 'Test1');
-                assert.equal(res.body.description, '12345678');
-                assert.equal(res.body._id, resource._id);
-                done(err);
-              });
+          assert.equal(res.body.title, 'Test1');
+          assert.equal(res.body.description, '12345678');
+          assert.equal(res.body._id, resource._id);
+          done(err);
         });
+    });
   });
 });
-
